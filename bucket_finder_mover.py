ea7c1b4a02b9f2750689805fe7e02386f09dc2ec
Lee Goodrich <goodrich.lee@gmail.com>
2021-08-03 09:48:14 -0700

    Organization and better names

--- bucket_finder_mover.py
+++ bucket_finder_mover.py
@@ -8,7 +8,7 @@ from distutils.dir_util import remove_tree
 bucket_list = []                                   # Need this empty list to start
 deduped_buckets = []                               # Need this empty list to start
 journal_fname = 'journal'                          # This should be whatever the current name of the journal files are
-frozen_index_path = './foo_index/'                 # Don't forget the trailing slash
+source_index = './foo_index/'                      # Don't forget the trailing slash
 dest_index = './bar_index/'                        # Don't forget the trailing slash
 search_string = 'source::WinEventLog:Security'     # The unique string to be used to find buckets
 
@@ -16,19 +16,19 @@ search_string = 'source::WinEventLog:Security'     # The unique string to be use
 # Build a list of all the current buckets in the frozen index, this way we know the number of buckets to iterate over
 def get_buckets():
     global bucket_list
-    bucket_list = os.listdir(frozen_index_path + 'frozendb/')
+    bucket_list = os.listdir(source_index + 'frozendb/')
     print('Starting frozen bucket count -> ' + str(len(bucket_list)))
 
 
-# Find all occcurences of the string match, build new list of matching buckets, dedup the final list
+# Find first occcurence of the string match, build new list of matching buckets, dedup the final list
 def find_matches():
-    matching_buckets = []
     count = 0
+    matching_buckets = []
     for bucket in bucket_list:
         try:
-            string_found = False
             count += 1
-            journal_path = (frozen_index_path + 'frozendb/' + bucket + '/rawdata/' + journal_fname)
+            string_found = False
+            journal_path = (source_index + 'frozendb/' + bucket + '/rawdata/' + journal_fname)
             print('Current matching bucket count is -> ' + str(len(matching_buckets)))
             print('Reading bucket ' + str(count) + ' of ' + str(len(bucket_list)) + ' in file ' + journal_path)
             with open(journal_path, 'r') as input_file:
@@ -55,7 +55,7 @@ def move_buckets():
     count = 0
     for bucket in deduped_buckets:
         count += 1
-        src = (frozen_index_path + 'frozendb/' + bucket)
+        src = (source_index + 'frozendb/' + bucket)
         dest = (dest_index + 'frozendb/' + bucket)
         print('Moving bucket ' + str(count) + ' of ' + str(len(deduped_buckets)) + ' from ' + src + ' to ' + dest + "\n")
         copy_tree(src, dest)
