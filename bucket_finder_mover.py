ce2ad6ecc57b4fdf89efe69d9271205049f6b9ea
Lee Goodrich <goodrich.lee@gmail.com>
2021-08-03 15:51:52 -0700

    Now with functioning arg parse

--- bucket_finder_mover.py
+++ bucket_finder_mover.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
-""" Splunk frozen bucket find and move script
+"""Splunk frozen bucket find and move script
+
 
 A simple script to search through a given index (directory) where frozen data
 has been written that actually needs to be in separate indexes. This solution
@@ -12,20 +13,16 @@ use this script to search all the journals, find all buckets that match, compile
 that list of directories, and then finally move them into the correct
 destination index.
 
-Requires two libraries, os and distutils
+Requires no third party libraries
 
-Main consists of three primary functions calls...
-
-get_buckets  - Builds a list of all buckets in the index. Gives us a finite list 
-               to iterate over for searching.
-find_matches - Finds the first occurence of search_string as it iterates over
-               all buckets in bucket_list. This yields a list of buckets to be
-               moved.
-move_buckets - Copies found buckets into the destination directory. If
-               succesful, remove the source from the old path.
+Main function consists of three primary functions calls:
+  get_buckets  - Builds a list of all buckets in the index. Gives us a finite list to iterate over for searching.
+  find_matches - Finds the first occurence of search_string as it iterates over all buckets in bucket_list. This yields a list of buckets to be moved.
+  move_buckets - Copies found buckets into the destination directory. If succesful, remove the source from the old path.
 """
 
 import os
+import argparse
 from distutils.dir_util import copy_tree
 from distutils.dir_util import remove_tree
 
@@ -33,55 +30,59 @@ from distutils.dir_util import remove_tree
 bucket_list = []                               # Need this empty list to start
 deduped_buckets = []                           # Need this empty list to start
 journal_fname = 'journal'                      # The name of your journal files
-source_index = './foo_index/'                  # Don't forget the trailing slash
-dest_index = './bar_index/'                    # Don't forget the trailing slash
-search_string = 'source::WinEventLog:Security' # Your unique search string
+
+
+def get_args():
+    """Get required arguments.
+    """
+
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(
+        'src_idx',
+        type = str,
+        help = "The absolute or relative path to the source frozen index"
+    )
+    parser.add_argument(
+        'dest_idx',
+        type = str,
+        help = "The absolute or relative path to the destination frozen index"
+    )
+    parser.add_argument(
+        'search_string',
+        type = str,
+        help = "The text chars you want to search for in all journal files"
+    )
+    args = parser.parse_args()
+    get_args.src_idx = args.src_idx
+    get_args.dest_idx = args.dest_idx
+    get_args.search_string = args.search_string 
 
 
 def get_buckets():
     """Builds a list of all buckets in the index.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    bucket_list
-        A list of all of the buckets in the index (directory)
     """
 
-    global bucket_list
-    bucket_list = os.listdir(source_index + 'frozendb/')
-    print('Starting frozen bucket count -> ' + str(len(bucket_list)))
+    get_buckets.bucket_list = os.listdir(get_args.src_idx + 'frozendb/')
+    print('Starting frozen bucket count -> ' + str(len(get_buckets.bucket_list)))
 
 
 def find_matches():
     """Finds all the buckets that have a matching string
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    deduped_buckets
-        A list of buckets containing files with matches to search_string
     """
 
     count = 0
     matching_buckets = []
-    for bucket in bucket_list:
+    for bucket in get_buckets.bucket_list:
         try:
             count += 1
             string_found = False
-            journal_path = (source_index + 'frozendb/' + bucket + '/rawdata/' + journal_fname)
+            journal_path = (get_args.src_idx + 'frozendb/' + bucket + '/rawdata/' + journal_fname)
             print('Current matching bucket count is -> ' + str(len(matching_buckets)))
-            print('Reading bucket ' + str(count) + ' of ' + str(len(bucket_list)) + ' in file ' + journal_path)
+            print('Reading bucket ' + str(count) + ' of ' + str(len(get_buckets.bucket_list)) + ' in file ' + journal_path)
             with open(journal_path, 'r') as input_file:
                 line_number = 0
                 for line in input_file:
-                    if string_found is False and search_string in line:
+                    if string_found is False and get_args.search_string in line:
                         line_number += 1
                         string_found = True
                         matching_buckets.append(bucket)
@@ -92,33 +93,25 @@ def find_matches():
         except:
             print('Issue with bucket -> ' + bucket + "\n")
             continue
-    global deduped_buckets
-    deduped_buckets = list(dict.fromkeys(matching_buckets))
-    print('Buckets found with matching string -> ' + str(len(deduped_buckets)) + "\n")
+    find_matches.deduped_buckets = list(dict.fromkeys(matching_buckets))
+    print('Buckets found with matching string -> ' + str(len(find_matches.deduped_buckets)) + "\n")
 
 
 def move_buckets():
     """Copies and removes source buckets if copy is successful
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    None
     """
 
     count = 0
-    for bucket in deduped_buckets:
+    for bucket in find_matches.deduped_buckets:
         count += 1
-        src = (source_index + 'frozendb/' + bucket)
-        dest = (dest_index + 'frozendb/' + bucket)
-        print('Moving bucket ' + str(count) + ' of ' + str(len(deduped_buckets)) + ' from ' + src + ' to ' + dest + "\n")
+        src = (get_args.src_idx + 'frozendb/' + bucket)
+        dest = (get_args.dest_idx + 'frozendb/' + bucket)
+        print('Moving bucket ' + str(count) + ' of ' + str(len(find_matches.deduped_buckets)) + ' from ' + src + ' to ' + dest + "\n")
         copy_tree(src, dest)
         remove_tree(src)
 
 def main():
+    get_args()
     get_buckets()
     print('Done Getting Buckets' + "\n")
     find_matches()
